7ceade99741dac6db76b204ca0c8d3bb7a34a293
Lana <memekoten@gmail.com>
2026-07-09 02:44:44 +1000

    Fixed an issue with silhouettes being drawn

--- Source/Core/DebugActions/OffsetEditor/Pawn_GetGizmos_DrawingOffsetEditor.cs
+++ Source/Core/DebugActions/OffsetEditor/Pawn_GetGizmos_DrawingOffsetEditor.cs
@@ -29,7 +29,8 @@ internal static class Pawn_GetGizmos_DrawingOffsetEditor
 #if DEBUG
         return pawn.RaceProps.Animal || ModSettings_GiddyUp.mechanoidsEnabled && pawn.RaceProps.IsMechanoid;
 #else
-        return DebugSettings.godMode && (pawn.RaceProps.Animal || ModSettings_GiddyUp.mechanoidsEnabled && pawn.RaceProps.IsMechanoid);
+        // return DebugSettings.godMode && (pawn.RaceProps.Animal || ModSettings_GiddyUp.mechanoidsEnabled && pawn.RaceProps.IsMechanoid);
+        return false;
 #endif
     }
 }
